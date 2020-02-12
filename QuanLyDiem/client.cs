c9e3e59c9b584ab820500c6171b41e262de25f89
lmd2610 <laminhduc2610@gmail.com>
2020-02-12 23:10:29 +0700

    update up data json

---
+++ QuanLyDiem/client.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text;
+
+
+namespace QuanLyDiem
+{
+    public class client
+    {
+        HttpClient _httpClient = null;
+        public HttpClient httpClient => _httpClient ?? (new HttpClient());
+        
+        public  async Task<string> SendAsyncJson(string url, string json)
+        {
+            
+            try
+            {
+
+                HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Post, url);
+                HttpContent httpContent = new StringContent(json, Encoding.UTF8, "application/json");
+                request.Content = httpContent;
+                var response = await httpClient.SendAsync(request);
+                var rcontent = await response.Content.ReadAsStringAsync();
+                return rcontent;
+
+            }
+            catch (Exception e)
+            {
+                throw e;
+                
+            }
+        }
+
+       
+    }
+}
--- ServerQuanLyDiemHocSinh/HttpServerClass.cs
+++ ServerQuanLyDiemHocSinh/HttpServerClass.cs
@@ -11,7 +11,7 @@ namespace ServerQuanLyDiemHocSinh
     class HttpServerClass
     {
         private HttpListener listener;
-
+        private string data;
         public HttpServerClass()
         {
             listener = new HttpListener();
@@ -21,10 +21,19 @@ namespace ServerQuanLyDiemHocSinh
         {
             listener.Prefixes.Add(prefix);
         }
+        public async void ShowReq()
+        {
+            HttpListenerContext context = listener.GetContext();
+            var body = new StreamReader(context.Request.InputStream).ReadToEnd();
+            data = body;
+            Console.WriteLine(body);
+
+        }
 
         public void Start()
         {
             listener.Start();
+            ShowReq();
             Respone();
         }
 
@@ -33,7 +42,7 @@ namespace ServerQuanLyDiemHocSinh
             while(true)
             {
                 HttpListenerContext context = listener.GetContext();
-                string msg = "hello world";
+                string msg = "Connected";
                 context.Response.ContentLength64 = Encoding.UTF8.GetByteCount(msg);
                 context.Response.StatusCode = (int)HttpStatusCode.OK;
                 using (Stream stream = context.Response.OutputStream)
